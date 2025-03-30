58df1afeee9ff8c9e998a77eb8515d1b12a35bd0
Oluwadamilola Adedeji <ebenoluwadamilola@gmail.com>
2025-03-30 17:09:43 +0100

    test

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -9,7 +9,7 @@ const about = () => {
       <Partner />
       <div className="lg:flex justify-center items-stretch  lg:ml-32 ml-8 mr-36 gap-48 p-8">
         <div className="flex flex-col justify-between">
-          <h1 className="font-[500] opacity-65 text-xl font-gayathri">About Us</h1>
+          <h1 className="font-[500] opacity-65 text-xl lg:mt-0 mt-48 font-gayathri">About Us</h1>
           <button className="relative flex items-center justify-between px-6 py-4 w-64 rounded-2xl bg-gradient-to-r from-[#D3BD00] from-10% via-[#F27405] to-[#5FB083] shadow-lg text-white font-medium backdrop-blur-4xl bg-opacity-50 border border-gray-500 font-dmSans">
             <span>Money Out</span>
             <span className="w-7 h-7 flex items-center justify-center rounded-full bg-black text-green-400 border border-gray-500 text-lg">
